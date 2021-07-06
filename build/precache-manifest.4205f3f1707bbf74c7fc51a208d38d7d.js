e19a868b3eb5f964690d070c5ef869eecf3f97f8
ABDULLA4G3 <abdulla4g3@sasi.ac.in>
2021-07-05 22:08:03 -0700

    initial deploy

---
+++ build/precache-manifest.4205f3f1707bbf74c7fc51a208d38d7d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9f512d1b8580ba67836",
+    "url": "/static/css/main.fc353efd.chunk.css"
+  },
+  {
+    "revision": "a9f512d1b8580ba67836",
+    "url": "/static/js/main.a9f512d1.chunk.js"
+  },
+  {
+    "revision": "99d7c59f45359249397b",
+    "url": "/static/js/1.99d7c59f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "30d675ab7d9895b4b46d08b175d0fb54",
+    "url": "/index.html"
+  }
+];
